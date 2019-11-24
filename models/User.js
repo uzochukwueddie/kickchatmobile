645fe76510ed9b4b4d805f488b1a103c9746f5b3
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2019-11-24 15:02:47 +0100

    display online users count for different rooms

--- models/User.js
+++ models/User.js
@@ -35,6 +35,7 @@ const userSchema = mongoose.Schema({
             senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
             message: { type: String },
             viewProfile: { type: Boolean, default: false },
+            likedPost: { type: Boolean, default: false },
             created: { type: Date, default: Date.now() },
             read: { type: Boolean, default: false },
             date: { type: String, default: '' }
--- socket/groupchat.js
+++ socket/groupchat.js
@@ -1,18 +1,25 @@
 module.exports = function(io, User, _) {
     const users = new User();
-
+    
     io.on('connection', (socket) => {
         socket.on('join', (params, callback) => {
             socket.join(params.room);
             
             users.EnterRoom(socket.id, params.sender.username, params.room, params.sender);
 
-            io.to(params.room).emit('usersList', users.GetUsersList(params.room));
+            const userData = users.GetUsersList(params.room);
+            io.to(params.room).emit('usersList', userData);
             
             let message = `This is the room for ${params.room} fans. Messages in this room are not saved. You can now start sending messages in this room.`
             io.to(params.room).emit('welcomeMsg', message)
             
             socket.join(params.sender.username);
+
+            const data = {
+                room: params.room,
+                numberOfConnections: userData.length
+            }
+            io.emit('globalchatroom', data);
             
             callback();
         });
@@ -28,18 +35,16 @@ module.exports = function(io, User, _) {
         socket.on('add-image', (message) => {
             io.to(message.room).emit('newGroupMessage', message);
         });
-        
-        // socket.on('request', (friend) => {
-        //     io.to(friend.receiver).emit('newFriend', {
-        //        from: friend.sender,
-        //        to: friend.receiver
-        //     }); 
-        // });
 
         socket.on('disconnect', () => {
             const user = users.RemoveUser(socket.id);
             if(user){
                 const userData = users.GetUsersList(user.room);
+                const data = {
+                    room: user.room,
+                    numberOfConnections: userData.length
+                }
+                io.emit('globalchatroom', data);
                 io.to(user.room).emit('usersList', userData);
             }
         })
